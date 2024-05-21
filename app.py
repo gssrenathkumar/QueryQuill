dece5559ce26f28c1888655c5a969909deb6eafe
G S SRENATH KUMAR <70089532+gssrenathkumar@users.noreply.github.com>
2024-05-21 17:23:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import os
-from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
