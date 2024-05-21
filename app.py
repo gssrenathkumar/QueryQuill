f5e6ebbc5fa6dcbaae97038e0f20246eba1ba4b7
G S SRENATH KUMAR <70089532+gssrenathkumar@users.noreply.github.com>
2024-05-21 17:24:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,6 @@ def handle_userinput(user_question,agent_executor):
 
 
 def main():
-    load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
